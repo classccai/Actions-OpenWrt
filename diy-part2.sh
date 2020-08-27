607d3f01f860d909fa134525a313d8fcfaf3071a
classccai <976385442@qq.com>
2020-08-27 22:30:25 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,6 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+# Modify tl-wr703n-v1 4m to 16m
+sed -i '/tl-wr703n-v1$/,/tl-wr703n-v1$/ s/4mlzma/16mlzma/g' target/linux/ar71xx/image/tiny-tp-link.mk
